017835191ee86fe3d544c7030568dfa95bc797f2
charlesCXK <715811763@qq.com>
2020-07-14 13:53:44 +0800

    update

--- SA-Gate.nyu/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-export NGPUS=8
-python -m torch.distributed.launch --nproc_per_node=$NGPUS train.py
-python eval.py -e 300-400 -d 0-7
